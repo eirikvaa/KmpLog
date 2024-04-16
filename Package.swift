130640a230d8faa123877a0837fdb65ee0960b33
Eirik Vale Aase <e.v.aase@gmail.com>
2024-04-16 22:33:36 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = ""
+let remoteKotlinUrl = "https://maven.pkg.github.com/airthings/kmplog/com/airthings/lib/kmplog-kmmbridge/0.2.6-spm.0/kmplog-kmmbridge-0.2.6-spm.0.zip"
 let remoteKotlinChecksum = ""
-let packageName = ""
+let packageName = "KmpLog"
 // END KMMBRIDGE BLOCK
 
 let package = Package(
